3f06610a5af09bdfc9c6cf2c5fe06cd0dacd85ec
kjmtsh <kjmtsh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-06 19:29:24 +0000

    Changed the version number.
    git-svn-id: https://plugins.svn.wordpress.org/sqlite-integration/trunk@799987 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sqlite-integration.php
+++ sqlite-integration.php
@@ -4,7 +4,7 @@ Plugin Name: SQLite Integration
 Plugin URI: http://dogwood.skr.jp/wordpress/sqlite-integration/
 Description: SQLite Integration is the plugin that enables WordPress to use SQLite. If you don't have MySQL and want to build a WordPress website, it's for you.
 Author: Kojima Toshiyasu
-Version: 1.4.1
+Version: 1.4.2
 Author URI: http://dogwood.skr.jp
 Text Domain: sqlite-integration
 Domain Path: /languages
